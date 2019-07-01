3cc32e733b83c5b6e3a70b46bd7a42f24e72dea6
Amir <amir.kashipazha@gmail.com>
2019-07-01 07:05:39 +0000

    stations table is done

--- createTablesDatabase.py
+++ createTablesDatabase.py
@@ -2,28 +2,7 @@ from databaseConnection import dataBaseConnect
 
 class createTabels:
     newConection = dataBaseConnect().connectToDataBase()
-    #print ( newConection.get_dsn_parameters(),"\n")
-    """
-    create table for station 
-    soruce file: ghcnd-stations.txt
-    """
-    cursor = newConection.cursor()
-    print("connected!")
-    stationTable = '''
-                    DROP TABLE IF EXISTS stations;
-                    CREATE TABLE stations (
-                        id serial PRIMARY KEY,
-                        noaa_id VARCHAR(20) UNIQUE NOT NULL,
-                        latitude real NOT NULL,
-                        longitude real NOT NULL,
-                        elevation real NOT NULL,
-                        state VARCHAR(20) NOT NULL,
-                        name VARCHAR(100) NOT NULL
-                    );
-                    '''
-    cursor.execute(stationTable)
-    print("station table created")
- 
+    cursor = newConection.cursor() 
 
     climateDataTableDaily = '''
                        DROP TABLE IF EXISTS stations_data_daily;
--- weather_stations.py
+++ weather_stations.py
@@ -1,13 +1,33 @@
 from constantValues.loginInfo import AwsLogins
 from databaseConnection import dataBaseConnect
-#from constantValues.databaseConnection import dataBaseConnect
-
 import boto3
 
 
 class createStationTable:
 
-   
+
+    def createStationsTable(self):
+        newConection = dataBaseConnect().connectToDataBase()
+        cursor = newConection.cursor()
+        stationTable = '''
+                    DROP TABLE IF EXISTS stations;
+                    CREATE TABLE stations (
+                        id serial PRIMARY KEY,
+                        noaa_id VARCHAR(20) UNIQUE NOT NULL,
+                        latitude real NOT NULL,
+                        longitude real NOT NULL,
+                        elevation real NOT NULL,
+                        state VARCHAR(20) NOT NULL,
+                        name VARCHAR(100) NOT NULL
+                    );
+                    '''
+        cursor.execute(stationTable)
+        print("station table created")
+        newConection.commit()
+        cursor.close()
+        newConection.close() 
+
+
     def addDataToTable(self):
         newConection = dataBaseConnect().connectToDataBase()
         cursor = newConection.cursor()
@@ -15,22 +35,20 @@ class createStationTable:
         connectAWS = AwsLogins()
         bucket = connectAWS.s3BucketClimate
         fileName = "ghcnd-stations.txt"
-
-        #bucket = connectAWS.s3Bucket
-        #fileName = "test-climate/ghcnd-stations.txt"
-
         lines = client.get_object(Bucket=bucket, Key=fileName)['Body'].read().decode('utf-8')
         lines = lines.splitlines()
-        #line = lines[0]
         for line in lines: 
             if line[0:2] == 'US':
-                #print(line[0:12], line[13:21], line[22:31], line[32:37], line[38:41], line[42:72] )
-                station = '''
-                         INSERT INTO stations (noaa_id, latitude, longitude, elevation, state, name)
-                         VALUES(%s,%s,%s,%s,%s,%s);
-                         '''
-                record_to_insert = (line[0:12], float(line[13:21]), float(line[22:31]), float(line[32:37]), line[38:41], line[42:72])
-                cursor.execute(station, record_to_insert)
+                stationName = line[42:72]
+                stationMetaData = [x for x in line[0:41].strip().split(' ') if x!='']
+                if stationMetaData[-1] != 'AK' and stationMetaData[-1] != 'HI' and float(stationMetaData[-2]) > 0:
+                    station = '''
+                             INSERT INTO stations (noaa_id, latitude, longitude, elevation, state, name)
+                             VALUES(%s,%s,%s,%s,%s,%s);
+                             '''
+                    record_to_insert = (stationMetaData[0], float(stationMetaData[1]), float(stationMetaData[2]),\
+                                        float(stationMetaData[3]), stationMetaData[4], stationName)
+                    cursor.execute(station, record_to_insert)
         newConection.commit()
         cursor.close()
         newConection.close()
@@ -38,5 +56,5 @@ class createStationTable:
 
 if __name__ == '__main__':
     stations = createStationTable()
+    stations.createStationsTable()
     stations.addDataToTable()
-
