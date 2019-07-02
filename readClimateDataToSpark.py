320efb879ba4e618cbff97481cb905c6177a6b83
Amir <amir.kashipazha@gmail.com>
2019-07-02 03:50:38 +0000

    code for table of elevation and all climate data is created

--- readClimateDataToSpark.py
+++ readClimateDataToSpark.py
@@ -7,6 +7,7 @@ from pyspark.sql.types import DateType
 from pyspark.sql import functions as F
 #from databaseConnection import connectToDataBase
 from constantValues.loginInfo import dataBaseLogin
+from databaseConnection import dataBaseConnect
 from pyspark.sql import DataFrameReader
 
 class readClimateData:
@@ -21,10 +22,49 @@ class readClimateData:
             .option("dbtable", "stations").load()
         return stationTable
 
+    def getClimateDataFromS3(self):
+        data_schema = StructType([StructField('station_name', StringType(), True),\
+                                  StructField('date', StringType(), True), \
+                                  StructField('data_type', StringType(), True),\
+                                  StructField('data', DoubleType(), True)])
+
+        climateData = self.spark.read.csv("s3a://noaa-ghcn-pds/csv/1900.csv", header=False, schema=data_schema)
+        climateData = climateData.groupby(climateData.station_name, climateData.date).pivot("data_type").avg("data")\
+                .select(['station_name', 'date', 'TMAX', 'TMIN', 'PRCP'])\
+                .withColumn('country', climateData['station_name'][0:2])
+        climateData = climateData.withColumn('TMAX', climateData['TMAX']/10)
+        climateData = climateData.withColumn('TMIN', climateData['TMIN']/10)
+        return climateData
+
+    def checkIfTableExist(self, tableName):
+        newConection = dataBaseConnect().connectToDataBase()
+        cursor = newConection.cursor()
+        ifExist = "DROP TABLE IF EXISTS " + tableName
+        cursor.execute(ifExist)
+        newConection.commit()
+        cursor.close()
+        newConection.close()
+
+
+    def createTableOfLower48(self):
+        psqlConnect = dataBaseLogin()
+        stations = self.getStationTable()
+        climateData = self.getClimateDataFromS3()
+        tableName = "lower48_data_elv"
+        self.checkIfTableExist(tableName)
+        lower48data = stations.join(climateData, stations.station_name == climateData.station_name)\
+                              .select(['id', 'date', 'elevation', 'TMAX', 'TMIN', 'PRCP'])
+        lower48data.write.format("jdbc").option("url",psqlConnect.url ).option("dbtable", tableName)\
+                   .option("user", psqlConnect.user).option("password", psqlConnect.password).save()
+
 
 
 if __name__ == '__main__':
     readingClimateData = readClimateData()
-    stationTable = readingClimateData.getStationTable()
-    stationTable.show()
+    #test = readingClimateData.getStationTable()
+    #test  = readingClimateData.getClimateDataFromS3()
+    test  = readingClimateData.createTableOfLower48()
+
+    #test.show()
+    print("Done!888888888----------------------=========================================000000000000000000000000000000000000")
 
